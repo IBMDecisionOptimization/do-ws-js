3bea08d5545d1c07c5e109c2f9637bbb1b42c348
Alain Chabrier <38245448+achabrier@users.noreply.github.com>
2019-02-13 09:43:00 +0100

    vega lite support

---
+++ public/scenario-vega-lite.js
@@ -0,0 +1,28 @@
+// requires
+//  <script src="https://cdn.jsdelivr.net/npm/vega@4.4.0"></script>
+// <script src="https://cdn.jsdelivr.net/npm/vega-lite@3.0.0-rc12"></script>
+// <script src="https://cdn.jsdelivr.net/npm/vega-embed@3.29.1"></script>
+
+function vegalitechart(containerId, table, config = undefined) {
+
+  data =[]
+
+  for (r in table.rows) {
+          let row = table.rows[r]
+          let d = {}
+          for (c in table.cols) {
+            let column = table.cols[c];
+            d[column] = row[column];
+          }
+          data.push(d);
+  }
+
+  if (config == undefined)
+    config = {};
+
+  config['$schema'] = 'https://vega.github.io/schema/vega-lite/v2.0.json';
+  config.data = { values: data };
+
+  vegaEmbed('#'+containerId, config);
+          
+}
